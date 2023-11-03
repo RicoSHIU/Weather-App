63f34b56ce68b2318c9a6f392aef54e905fd8acc
RicoSHIU <148872048+RicoSHIU@users.noreply.github.com>
2023-11-04 00:07:15 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// prompt
+//alert
+
+// alert("Hello World!")
+let myName = prompt ("what is your name?")
+
+
+// button querry selector
+let myButton = document.querySelector("#myButton");
+
+// find element by id
+// find element by class
+
+//add event listener
+myButton.addEventListener("click", myClick);
+
+function myClick() {
+    // console.log('Clicking!')
+    // console.log(myName);
+alert("Hello" + myName + "!")
+// What is Strings
+// "fgfdhgtstthgtf"
+// 'gfdthgfhyfyhyt'
+// "123456790"
+// 'dsadggszhgfh'
+alert('Hello $(myName)!')
+
+const weather = prompt ('How is the weather today');
+
+}
+
+// let可以改 const不可以改
+// changed -> contnot use const
+letsum = 100;
+sun = sun +10;
+console.log("sum: ", sum)
